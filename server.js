ad03d2e1ea4a837b1c30e93262cca6ac765154ea
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-12-30 02:54:57 +0000

    TweetScanners are now created by, and controllalble from, the webapp
    - Added statistics and additional info to TweetScanner
    - Added TweetScanner setPollInterval() method
    - TweetScanners are initialised at server startup and created for newly-registered users
    - /controlpanel can now be used by a logged-in user to control their TweetScanner

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-var express = require('express')
+var util = require('util')
+
+var async = require('async')
+  , express = require('express')
   , OAuth= require('oauth').OAuth
-  , RedisStore = require('connect-redis')(express)
-  , settings = require('./settings')
   , redis = require('redis').createClient()
-  , util = require('util')
+  , RedisStore = require('connect-redis')(express)
+
+var settings = require('./settings')
   , substitute = require('./index')
+  , pluralise = require('./lib/util').pluralise
 
 redis.on('error', function (err) {
-    console.error('Redis Error: %s', err)
+  console.error('Redis Error: %s', err)
 })
 
 var oa = new OAuth(
@@ -21,6 +25,53 @@ var oa = new OAuth(
 )
 
 var app = express.createServer()
+/** userId -> TweetScanner */
+var scanners = {}
+
+/**
+ * Initialises a TweetScanner for each registered user.
+ */
+function initialiseScanners(cb) {
+  redis.smembers('users', function(err, userIds) {
+    if (err) {
+      console.error('Error loading user ids: %s', err)
+      return cb(err)
+    }
+
+    async.map(
+      userIds
+    , function(userId, cb) {
+        redis.hgetall(util.format('user:%s', userId), function(err, user) {
+          if (err) {
+            return cb(err)
+          }
+          cb(null, user)
+        })
+      }
+    , function(err, users) {
+        if (err) {
+          console.error('Error initialising scanners: %s', err)
+          return cb(err)
+        }
+        users.forEach(createScanner)
+        cb(null, users.length)
+      }
+    )
+  })
+}
+
+/**
+ * Initialises a TweetScanner for the given user.
+ */
+function createScanner(user) {
+  scanners[user.id] = new substitute.TweetScanner(user.screenName, {
+    consumer_key: settings.consumerKey
+  , consumer_secret: settings.consumerSecret
+  , access_token_key: user.accessKey
+  , access_token_secret: user.accessSecret
+  })
+  return scanners[user.id]
+}
 
 // ---------------------------------------------------------- Express Config ---
 
@@ -76,11 +127,15 @@ app.dynamicHelpers({
 
 // ------------------------------------------------------------- URL Mapping ---
 
-app.get('/',                      index)
-app.get('/auth/twitter',          twitterAuth)
-app.get('/auth/twitter/callback', twitterAuthCallback)
-app.get('/controlpanel',          userControlPanel)
-app.get('/expressions',           expressions)
+app.get('/',                       index)
+app.get('/auth/twitter',           twitterAuth)
+app.get('/auth/twitter/callback',  twitterAuthCallback)
+app.get('/controlpanel',           userControlPanel)
+app.post('/controlpanel/stop',     stopScanner)
+app.post('/controlpanel/start',    startScanner)
+app.post('/controlpanel/interval', changePollInterval)
+app.get('/expressions',            expressions)
+app.all('/expressions/test',       testExpressions)
 
 // Register a catch-all handler to render 404.jade
 app.use(function(req, res, next) {
@@ -97,16 +152,10 @@ function twitterAuth(req, res, next) {
   oa.getOAuthRequestToken(function(err, token, secret, results) {
     if (err) {
       console.error(err)
-      next(err)
+      return next(err)
     }
-    else {
-      req.session.oauth = {}
-      req.session.oauth.token = token
-      console.info('oauth.token: %s', req.session.oauth.token)
-      req.session.oauth.secret = secret
-      console.info('oauth.secret: %s', req.session.oauth.secret)
-      res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + token)
-    }
+    req.session.oauth = { token: token, secret: secret }
+    res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + token)
   })
 }
 
@@ -115,25 +164,27 @@ function twitterAuthCallback(req, res, next) {
     return next(new Error('Twitter authorisation not in progress.'))
   }
 
-  req.session.oauth.verifier = req.query.oauth_verifier
   var oauth = req.session.oauth
-
-  oa.getOAuthAccessToken(oauth.token, oauth.secret, oauth.verifier,
-    function(err, accessToken, accessSecret, results) {
+  oa.getOAuthAccessToken(oauth.token, oauth.secret, req.query.oauth_verifier,
+    function(err, accessKey, accessSecret, results) {
       if (err) {
         console.error(err)
         return next(err)
       }
 
       var userId = results.user_id
-      redis.hmset(util.format('user:%s', userId), {
-        id: userId
-      , screenName: results.screen_name
-      , accessToken: accessToken
-      , accessSecret: accessSecret
-      })
+        , user = { id: userId
+                 , screenName: results.screen_name
+                 , accessKey: accessKey
+                 , accessSecret: accessSecret }
+      redis.sadd('users', userId)
+      redis.hmset(util.format('user:%s', userId), user)
       req.session.userId = userId
       delete req.session.oauth
+      if (!scanners.hasOwnProperty(userId)) {
+        createScanner(user)
+        console.info('Created new TweetScanner for %s', user.screenName)
+      }
       res.redirect('/controlpanel')
     }
   )
@@ -143,12 +194,68 @@ function userControlPanel(req, res, next) {
   if (!req.user.isAuthenticated) {
     return next(new Error('You must be signed in to view this page.'))
   }
-  res.render('controlpanel')
+  // TODO Scanner settings Form for validation and redisplay
+  res.render('controlpanel', {
+    scanner: scanners[req.user.id]
+  })
+}
+
+function stopScanner(req, res, next) {
+  if (!req.user.isAuthenticated) {
+    return next(new Error('You must be signed in to view this page.'))
+  }
+  var scanner = scanners[req.user.id]
+  if (scanner.running) {
+    scanner.stop()
+    redis.set(util.format('scanner:%s', req.user.id), 'stopped')
+  }
+  res.redirect('/controlpanel')
+}
+
+function startScanner(req, res, next) {
+  if (!req.user.isAuthenticated) {
+    return next(new Error('You must be signed in to view this page.'))
+  }
+  var scanner = scanners[req.user.id]
+  if (!scanner.running) {
+    scanner.start()
+    redis.set(util.format('scanner:%s', req.user.id), 'running')
+  }
+  res.redirect('/controlpanel')
+}
+
+function changePollInterval(req, res, next) {
+  if (!req.user.isAuthenticated) {
+    return next(new Error('You must be signed in to view this page.'))
+  }
+  var scanner = scanners[req.user.id]
+  // TODO Scanner settings Form for validation and redisplay
+  var pollInterval = parseInt(req.body.pollInterval, 10)
+  if (pollInterval &&
+      pollInterval % 1000 === 0 && // Whole seconds
+      pollInterval >= 60000 &&     // 1 minute
+      pollInterval <= 900000) {    // 15 minutes
+    scanner.setPollInterval(pollInterval)
+  }
+  res.redirect('/controlpanel')
 }
 
 function expressions(req, res) {
   res.render('expressions')
 }
 
-app.listen(settings.port, '0.0.0.0')
-console.log('Substitute server listening on ' + settings.domain)
+function testExpressions(req, res) {
+  // TODO
+  res.render('testexpressions')
+}
+
+// ---------------------------------------------------------- Server Startup ---
+
+initialiseScanners(function(err, count) {
+  if (err) {
+    return console.errror('Substitute server not started.')
+  }
+  console.info('Initialised %s TweetScanner%s', count, pluralise(count))
+  app.listen(settings.port, '0.0.0.0')
+  console.log('Substitute server listening on ' + settings.domain)
+})
--- test/tweetscanner.js
+++ test/tweetscanner.js
@@ -1,4 +1,5 @@
-var sinon = require('sinon')
+var assert = require('assert')
+  , sinon = require('sinon')
   , TweetScanner = require('../lib/tweetscanner')
 
 describe('TweetScanner', function() {
@@ -10,6 +11,34 @@ describe('TweetScanner', function() {
       , access_token_key: 'ak'
       , access_token_secret: 'as'
       }
+
+  describe('new(user, auth)', function() {
+    before(function() {
+      scanner = new TweetScanner(testUser, testAuth)
+    })
+    it('should have a default pollInterval of 60 seconds', function() {
+      assert.equal(scanner.pollInterval, 60000)
+    })
+    it('should not start running by default', function() {
+      assert.strictEqual(scanner.previousTweet, null)
+      assert.strictEqual(scanner.running, false)
+      assert.strictEqual(scanner.intervalId, null)
+    })
+    it('should have zero/null statistics', function() {
+      assert.strictEqual(scanner.lastRunAt, null)
+      assert.strictEqual(scanner.tweetsScanned, 0)
+      assert.strictEqual(scanner.substitutionsMade, 0)
+    })
+  })
+
+  describe('new(user, auth, pollInterval)', function() {
+    before(function() {
+      scanner = new TweetScanner(testUser, testAuth, 120000)
+    })
+    it('should override the default pollInterval', function() {
+      assert.equal(scanner.pollInterval, 120000)
+    })
+  })
 
   describe('correctTweet()', function() {
     // Test data
@@ -40,6 +69,8 @@ describe('TweetScanner', function() {
         sinon.assert.calledWith(destroyStatus, originalTweet.id_str)
         sinon.assert.calledWith(destroyStatus, subTweet.id_str)
         sinon.assert.callOrder(updateStatus, destroyStatus)
+        assert.strictEqual(scanner.substitutionsMade, 1,
+                           'substitutionsMade should have been bumped up')
         done()
       })
     })
@@ -52,6 +83,8 @@ describe('TweetScanner', function() {
           in_reply_to_status_id: originalTweet.in_reply_to_status_id_str
         })
         sinon.assert.notCalled(destroyStatus)
+        assert.strictEqual(scanner.substitutionsMade, 0,
+                           'substitutionsMade should not have been bumped up')
         done()
       })
     })
